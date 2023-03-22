c3e99fd3d8654fca2f15c0dbb4a6b43a2e6a6e61
ddrraaggaarriiss <okay969696@gmail.com>
2023-03-22 22:49:13 +0300

    hw4

--- handlers/user_info_fsm.py
+++ handlers/user_info_fsm.py
@@ -1,41 +1,99 @@
-# from aiogram import types
-# from aiogram.dispatcher import FSMContext
-# from aiogram.dispatcher.filters.state import State, StatesGroup
-#
-#
-# # FSM - Finite State Mashine
-# class UserForm(StatesGroup):
-#     name = State()
-#     age = State()
-#     address = State()
-#
-#
-# async def start_form(message: types.Message):
-#     await UserForm.name.set()
-#     await message.answer("Как вас зовут?")
-#
-#
-# async def process_name(message: types.Message, state: FSMContext):
-#     async with state.proxy() as data:
-#         data['name'] = message.text
-#     await UserForm.next()
-#     await message.answer("Сколько вам лет?")
-#
-#
-# async def process_age(message: types.Message, state: FSMContext):
-#     # if not message.text.isdigit():
-#     #     await message.answer("ВВедите только цифры")
-#     # else:
-#     async with state.proxy() as data:
-#         data['age'] = message.text
-#     await UserForm.next()
-#     await message.answer("Какой у вас адрес?")
-#
-#
-# async def process_address(message: types.Message, state: FSMContext):
-#     async with state.proxy() as data:
-#         data['address'] = message.text
-#         print(data)
-#
-#     await state.finish()
-#     await message.answer("Спасибо.")
+
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+# FSM - Finite State Mashine
+class UserForm(StatesGroup):
+    name = State()
+    age = State()
+    address = State()
+    delivery_day = State()
+
+
+
+async def start_form(message: types.Message):
+    """
+          Фуекция для старта форума
+    """
+    await UserForm.name.set()
+    await message.answer("Введите ваше имя")
+
+
+
+async def process_name(message: types.Message, state: FSMContext):
+    """
+          Обработчик ответа на вопрос "Введите ваше имя"
+    """
+    async with state.proxy() as data:
+        data['name'] = message.text # Сохраняем имя в данных
+    await UserForm.next() # Переходим к следующему состоянию (возраст)
+    await message.answer("Введите ваш возраст")
+
+
+
+async def process_age(message: types.Message, state: FSMContext):
+    """
+        Обработчик ответа на вопрос "Введите ваш возраст" и валидация (проверка) если данные не числы
+    """
+    if not message.text.isdigit():
+        return await message.answer("Пожалуйста, введите возраст цифрами.")
+    async with state.proxy() as data:
+        data['age'] = int(message.text) # Сохраняем возраст в данных
+    await UserForm.next() # Переходим к следующему состоянию (адрес)
+    await message.answer("Введите ваш адрес")
+
+
+
+async def process_address(message: types.Message, state: FSMContext):
+    """
+        Обработчик ответа на вопрос "Введите ваш адрес"
+    """
+    kb = types.ReplyKeyboardMarkup()  # кнопки подсказки для дней недели
+    kb.add(types.KeyboardButton('Понедельник'),
+           types.KeyboardButton('Вторник'), types.KeyboardButton('Среда'),
+           types.KeyboardButton('Четверг'), types.KeyboardButton('Пятница'),
+           types.KeyboardButton('Субботa'), types.KeyboardButton('Воскресенье'))
+    async with state.proxy() as data:
+        data['address'] = message.text # Сохраняем адрес в данных
+    await UserForm.next()  # Переходим к следующему состоянию (день недели)
+    await message.reply("Выберите день, который вам удобен для получения товара", reply_markup=kb)
+
+
+
+async def process_delivery_day(message: types.Message, state: FSMContext):
+    """"
+        Обработчик ответа на вопрос "Выберите день, который вам удобен для получения товара"
+    """
+    async with state.proxy() as data:
+        data['delivery_day'] = message.text.strip() # Сохраняем день в данных
+        print(data)
+    await message.answer(
+        f"Спасибо за заполнение анкеты.\n"
+        f"Имя: {data['name']}\n"
+        f"Возраст: {data['age']}\n"
+        f"Адрес: {data['address']}\n"
+        f"День доставки: {data['delivery_day']}")
+    await state.finish()
+    await message.answer(f"Ваш заказ будет доставлен\nпо адресу: {data['address']} \nдень недели: {data['delivery_day']}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,37 +2,47 @@ from aiogram import executor
 import logging
 from aiogram.dispatcher.filters import Text
 
+# hw 1
 from config import dp
-from handlers.basic_handlers import (help,myinfo,picture)
+from handlers.basic_handlers import (help, myinfo, picture)
+# hw 2
 from handlers.shop import (show_categories,show_category_clothes,
-                           show_category_clothes_callback,
-                           show_category_shoes,
-                           show_category_shoes_callback,show_address)
+                           show_category_clothes_callback, show_category_shoes,
+                           show_category_shoes_callback, show_address)
 
-
-from handlers.admin import (check_curses,ban_user,pin_message)
-
+# hw 3
+from handlers.admin import (check_curses, ban_user, pin_message)
+# hw 4
+from handlers.user_info_fsm import (start_form, process_name, process_age,
+                                    process_address, UserForm, process_delivery_day)
 
 
 
 
 
 if __name__ == "__main__":
+    # hw 1
     logging.basicConfig(level=logging.INFO)
-    dp.register_message_handler(show_categories,commands=["start"])
+    dp.register_message_handler(show_categories, commands=["start"])
     dp.register_message_handler(show_category_clothes, Text(equals="Одежды"))
     dp.register_callback_query_handler(show_category_clothes_callback, Text(equals='jackets'))
     dp.register_message_handler(show_category_shoes, Text(equals="Обувь"))
     dp.register_callback_query_handler(show_category_shoes_callback, Text(equals='slippers'))
     dp.register_message_handler(show_address, Text(equals="Адрес"))
-
+    # hw 2
     dp.register_message_handler(help, commands=["help"])
     dp.register_message_handler(myinfo, commands=["myinfo"])
     dp.register_message_handler(picture, commands=["picture"])
-
+    # hw 3
     dp.register_message_handler(pin_message, commands=["pin"], commands_prefix="/")
-    dp.register_message_handler(ban_user, commands=["да"],commands_prefix=['/'])
-    dp.register_message_handler(check_curses)
+    dp.register_message_handler(ban_user, commands=["да"], commands_prefix=['/'])
+    # hw 4
+    dp.register_message_handler(start_form, commands=["form"])
+    dp.register_message_handler(process_name, state=UserForm.name)
+    dp.register_message_handler(process_age, state=UserForm.age)
+    dp.register_message_handler(process_address, state=UserForm.address)
+    dp.register_message_handler(process_delivery_day, state=UserForm.delivery_day)
 
+    dp.register_message_handler(check_curses)
     executor.start_polling(dp)
 
